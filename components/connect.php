aeaab00fb334361d05863b8bef2dbba10d63205f
Choomai <nguyencaonguyen0944322545@gmail.com>
2024-10-29 15:20:19 +0700

    4 spaces

--- components/connect.php
+++ components/connect.php
@@ -1,16 +1,16 @@
 <?php
-   $env = parse_ini_file("../.env");
+    $env = parse_ini_file("../.env");
 
-   $conn = new mysqli($env["DB_HOST"], $env["DB_USER"], $env["DB_PASSWORD"], $env["DB_NAME"], null, $env["DB_SOCKET"]);
+    $conn = new mysqli($env["DB_HOST"], $env["DB_USER"], $env["DB_PASSWORD"], $env["DB_NAME"], null, $env["DB_SOCKET"]);
 
-   function unique_id() {
-      $str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-      $rand = array();
-      $length = strlen($str) - 1;
-      for ($i = 0; $i < 20; $i++) {
-          $n = mt_rand(0, $length);
-          $rand[] = $str[$n];
-      }
-      return implode($rand);
-   }
+    function unique_id() {
+        $str = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+        $rand = array();
+        $length = strlen($str) - 1;
+        for ($i = 0; $i < 20; $i++) {
+            $n = mt_rand(0, $length);
+            $rand[] = $str[$n];
+        }
+        return implode($rand);
+    }
 ?>
